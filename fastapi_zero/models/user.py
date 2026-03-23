38c2df4a294798915e8f9b87b342011f8d6c4d17
andreaqueiroz1307-sap <andreaqueiroz1307@gmail.com>
2026-03-23 19:51:57 -0300

    Atualização nos testes e endpoints

---
+++ fastapi_zero/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from fastapi_zero.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    senha = Column(String, nullable=False)
+    tarefas = relationship("Task", back_populates="usuario")
--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -1,19 +1,80 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient  # recurso da propria FastAPI
 
-from fastapi_zero.app import app  # importando da pasta o nosso arquivo
-
-
-def test_root_retorna_ola_mundo():  # toda função em teste começa com test_
-
-    # arrange
-    client = TestClient(app)  # cliente de teste do FastAPI
-
-    # act - execução (SUT - system under test)
+def test_root_retorna_mensagem(client):
     response = client.get("/")
-
-    # assert - garantir que A = A
     assert response.json() == {"message": "API funcionando"}
-    # garante que veio a resposta que esperava
     assert response.status_code == HTTPStatus.OK
+
+
+def test_criar_user(client):
+    response = client.post(
+        "/users",
+        json={
+            "nome": "Andrea",
+            "email": "AandreaQueiroz1307@gmail.com",
+            "senha": "1234##",
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "nome": "Andrea",
+        "email": "AandreaQueiroz1307@gmail.com",
+    }
+
+
+def test_listar_users(client):
+    response = client.get("/users")
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_criar_task(client):
+    response = client.post(
+        "/tarefas",
+        json={
+            "titulo": "Estudar API",
+            "descricao": "Fazer a API em python",
+            "prioridade": "alta",
+            "user_id": 1,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "titulo": "Estudar API",
+        "descricao": "Fazer a API em python",
+        "prioridade": "alta",
+        "concluida": False,
+    }
+
+
+def test_listar_tarefas(client):
+    response = client.get("/tarefas")
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_atualizar_user(client):
+
+    response = client.post(
+        "/users",
+        json={
+            "nome": "Andrea",
+            "email": "teeste@teste.com",
+            "senha": "1234##",
+        },
+    )
+
+    response = client.put(
+        "/users/1",
+        json={
+            "nome": "Andrea Queiroz",
+            "email": "aandrea@teste.com",
+            "senha": "1234##",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "nome": "Andrea Queiroz",
+        "email": "aandrea@teste.com",
+    }
